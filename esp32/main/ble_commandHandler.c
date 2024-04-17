d183d9652fefcb94fe5b6bd8d5c4cee3145c15e9
Alexander Nørup (WSL) <me@alexandernorup.com>
2024-04-17 13:15:49 +0200

    Cleanup

--- esp32/main/ble_commandHandler.c
+++ esp32/main/ble_commandHandler.c
@@ -42,7 +42,6 @@ void handle_ble_message(uint8_t *buffer, uint16_t len){
         return;
     }
 
-    // TOOD: Implement request-lock mode button
     if(cmd.requestedCommand == 0x13){
         ESP_LOGI(TAG, "Recieved open command! Now from the commandHandler");
         set_door_state(1);
