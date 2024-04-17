4794ec5a15db1bbae523794f9fe5c5f624d6444e
Alexander Nørup (WSL) <me@alexandernorup.com>
2024-04-17 13:01:30 +0200

    Set priority 1 for HTTP task

--- esp32/main/ble_controller.c
+++ esp32/main/ble_controller.c
@@ -664,5 +664,5 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    xTaskCreate(run_heartbeat_client, "HeartbeatClient", 4096, NULL, 5, NULL);
+    xTaskCreate(run_heartbeat_client, "HeartbeatClient", 4096, NULL, 1, NULL);
 }
