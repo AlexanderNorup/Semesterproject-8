46fa288332660bfb314f390a75adf6c632e42c19
Victor <35064405+VictorABoye@users.noreply.github.com>
2024-05-06 13:06:23 +0200

    Update web_server.py
    Add alarm that sounds when the heartbeat server has not received heartbeats

--- hbserver/web_server.py
+++ hbserver/web_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import time
 import threading
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
@@ -10,20 +11,385 @@ last_post_time = 0
 is_dead = False
 already_printed_dead_message = False
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+buzzerPin = 13
+GPIO.setup(buzzerPin, GPIO.OUT)
+
+buzzer_pwm = GPIO.PWM(buzzerPin, 500)
+
+
+
+
+def delay(x):
+    global is_dead
+    time.sleep(0.2)
+    buzzer_pwm.ChangeDutyCycle(0)
+    if (not is_dead):
+        print("stop sounding the alarm, lol")
+        raise Exception("hehehe")
+    time.sleep(max(x * 0.001 - 0.2, 0))
+    buzzer_pwm.ChangeDutyCycle(99)
+
+def tone(pin, freq):
+    buzzer_pwm.ChangeFrequency(freq+750)
+
 def is_alive():
     global last_post_time
     # Check if the last post was within the last 30 seconds
     return time.time() - last_post_time < 30
+
+def sound():
+
+    buzzer_pwm.start(0)
+
+    print("SOUND ALARM!1!1!")
+    try:
+        print("SOUNDING THE ALARM")
+        buzzer_pwm.ChangeDutyCycle(99)
+        tone(buzzerPin,44)
+
+        delay(528)
+        tone(buzzerPin,466)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(528)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,494)
+
+        delay(211)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(528)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(106)
+        tone(buzzerPin,466)
+
+        delay(106)
+        tone(buzzerPin,494)
+
+        delay(528)
+        tone(buzzerPin,466)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(528)
+        tone(buzzerPin,466)
+
+        delay(106)
+        tone(buzzerPin,494)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(528)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(211)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(106)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(528)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(211)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,349)
+
+        delay(528)
+        tone(buzzerPin,330)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(211)
+        tone(buzzerPin,330)
+
+        delay(106)
+        tone(buzzerPin,349)
+
+        delay(528)
+        tone(buzzerPin,330)
+
+        delay(106)
+        tone(buzzerPin,349)
+
+        delay(106)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,349)
+
+        delay(528)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,659)
+
+        delay(282)
+        tone(buzzerPin,587)
+
+        delay(282)
+        tone(buzzerPin,523)
+
+        delay(282)
+        tone(buzzerPin,494)
+
+        delay(634)
+        tone(buzzerPin,392)
+
+        delay(1056)
+        tone(buzzerPin,392)
+
+        delay(282)
+        tone(buzzerPin,523)
+
+        delay(282)
+        tone(buzzerPin,587)
+
+        delay(282)
+        tone(buzzerPin,494)
+
+        delay(634)
+        tone(buzzerPin,392)
+
+        delay(1056)
+        tone(buzzerPin,494)
+
+        delay(528)
+        tone(buzzerPin,466)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(528)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,494)
+
+        delay(211)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(528)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(106)
+        tone(buzzerPin,466)
+
+        delay(106)
+        tone(buzzerPin,494)
+
+        delay(528)
+        tone(buzzerPin,466)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(528)
+        tone(buzzerPin,466)
+
+        delay(106)
+        tone(buzzerPin,494)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(528)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(211)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(106)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(528)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,415)
+
+        delay(106)
+        tone(buzzerPin,440)
+
+        delay(211)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,349)
+
+        delay(528)
+        tone(buzzerPin,330)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(211)
+        tone(buzzerPin,330)
+
+        delay(106)
+        tone(buzzerPin,349)
+
+        delay(528)
+        tone(buzzerPin,330)
+
+        delay(106)
+        tone(buzzerPin,349)
+
+        delay(106)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,349)
+
+        delay(528)
+        tone(buzzerPin,370)
+
+        delay(106)
+        tone(buzzerPin,392)
+
+        delay(528)
+        tone(buzzerPin,659)
+
+        delay(282)
+        tone(buzzerPin,587)
+
+        delay(282)
+        tone(buzzerPin,523)
+
+        delay(282)
+        tone(buzzerPin,494)
+
+        delay(634)
+        tone(buzzerPin,392)
+
+        delay(1056)
+        tone(buzzerPin,392)
+
+        delay(282)
+        tone(buzzerPin,523)
+
+        delay(282)
+        tone(buzzerPin,587)
+
+        delay(282)
+        tone(buzzerPin,494)
+
+        delay(634)
+        tone(buzzerPin,392)
+    except Exception:
+        print("stopping alarm")
+    finally:
+        print("final")
 
 def check_alive():
     global is_dead, already_printed_dead_message
     time.sleep(15)
     while True:
         if not is_alive():
+            is_dead = True
             if not already_printed_dead_message:
                 print("Server is dead!")
                 already_printed_dead_message = True
-            is_dead = True
+                sound()
         else:
             is_dead = False
             already_printed_dead_message = False
@@ -35,11 +401,11 @@ alive_checker_thread.start()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global current_state, last_post_time
+    global current_state, last_post_time, is_dead
     
     if request.method == 'POST':
-        state = request.form.get('state')
-        
+        state = request.form.get('doorstate')
+        is_dead = False
         # Update current state and time
         current_state = "Unlocked" if state.strip() == "1" else "Locked"
         last_post_time = time.time()
