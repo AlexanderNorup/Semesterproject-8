3eb0c94f7dc295fa4861112b925ee677146bf94c
Victor <35064405+VictorABoye@users.noreply.github.com>
2024-05-06 13:22:52 +0200

    Update web_server.py
    Updated print statements

--- hbserver/web_server.py
+++ hbserver/web_server.py
@@ -26,7 +26,6 @@ def delay(x):
     time.sleep(0.2)
     buzzer_pwm.ChangeDutyCycle(0)
     if (not is_dead):
-        print("stop sounding the alarm, lol")
         raise Exception("hehehe")
     time.sleep(max(x * 0.001 - 0.2, 0))
     buzzer_pwm.ChangeDutyCycle(99)
@@ -39,13 +38,11 @@ def is_alive():
     # Check if the last post was within the last 30 seconds
     return time.time() - last_post_time < 30
 
+
 def sound():
 
     buzzer_pwm.start(0)
-
-    print("SOUND ALARM!1!1!")
     try:
-        print("SOUNDING THE ALARM")
         buzzer_pwm.ChangeDutyCycle(99)
         tone(buzzerPin,44)
 
@@ -377,8 +374,6 @@ def sound():
         tone(buzzerPin,392)
     except Exception:
         print("stopping alarm")
-    finally:
-        print("final")
 
 def check_alive():
     global is_dead, already_printed_dead_message
@@ -387,7 +382,7 @@ def check_alive():
         if not is_alive():
             is_dead = True
             if not already_printed_dead_message:
-                print("Server is dead!")
+                print("Doorlock is dead!")
                 already_printed_dead_message = True
                 sound()
         else:
@@ -398,6 +393,7 @@ def check_alive():
 alive_checker_thread = threading.Thread(target=check_alive)
 alive_checker_thread.daemon = True
 alive_checker_thread.start()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -418,3 +414,6 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
+
+
